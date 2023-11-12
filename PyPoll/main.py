ba89c567825602730a2e675b188d82c77e698780
stephanielabelle <stephanie.labelle14@gmail.com>
2023-11-11 23:24:53 -0500

    figured out tally

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,12 +7,11 @@ csvpath = os.path.join('PyPoll','Resources','election_data.csv')
 print(csvpath)
 
 candidate = []
-
-candidates = []
+total_can_list = []
 num_votes_c0 = 0
 num_votes_c1 = 0
 num_votes_c2 = 0
-num=1
+
 
 with open(csvpath,"r") as pollcsv:
     csvreader = csv.reader(pollcsv, delimiter=',')
@@ -20,19 +19,35 @@ with open(csvpath,"r") as pollcsv:
 
     
     for row in csvreader: 
-        
+        total_can_list.append(row[2])
         if row[2] not in candidate:
             candidate.append(row[2])
         
         #counts number of ballots 
         ballot_count += 1
 
-        if row[2] == candidate[0]:
-            num_votes_c0 += 1
-        elif row[2] == candidate[1]:
-            num_votes_c1 += 1
-        else:
-            num_votes_c2 += 1
+tally = []
+tallyentry = 0
+length = len(candidate)
+print(length)
+for x in range(0,length):
+    name = candidate[x]
+    print(x)
+    print(f'name in x {name}')
+    for entry in total_can_list:
+        if entry == name:
+            tallyentry +=1
+    tally.append(tallyentry)
+    tallyentry=0
+        
+line1 = 'Election Results'
+
+        # if row[2] == candidate[0]:
+        #     num_votes_c0 += 1
+        # elif row[2] == candidate[1]:
+        #     num_votes_c1 += 1
+        # else:
+        #     num_votes_c2 += 1
 
 print(f'{candidate[0]} {num_votes_c0}')
 print(f'{candidate[1]} {num_votes_c1}')
